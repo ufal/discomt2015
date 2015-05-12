6d12c35cd23d3ad510e3d850b8a2c7d8bae7ca90
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-05-12 10:41:15 +0200

    bugfix: count also the trailing empty string

--- class/scripts/vw_res_to_official_res.pl
+++ class/scripts/vw_res_to_official_res.pl
@@ -23,7 +23,7 @@ open my $fh_orig, "<:gzip:utf8", $orig_data;
 while (my $orig_line = <$fh_orig>) {
     chomp $orig_line;
     my ($class, $miss_word, $en_sent, $fr_sent, $ali) = split /\t/, $orig_line;
-    my $replace_num = scalar(split /REPLACE_[0-9]+/, $fr_sent) - 1;
+    my $replace_num = scalar(split /REPLACE_[0-9]+/, $fr_sent, -1) - 1;
 
     my @scores_per_instance = ();
     while ($replace_num && (my $res_line = <STDIN>)) {
