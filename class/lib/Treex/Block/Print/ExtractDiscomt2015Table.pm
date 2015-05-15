c762f00e939e7a209450623065e1b7e525c8c8e8
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-05-15 02:18:57 +0200

    plenty of new features added

--- class/lib/Treex/Block/Print/ExtractDiscomt2015Table.pm
+++ class/lib/Treex/Block/Print/ExtractDiscomt2015Table.pm
@@ -27,6 +27,13 @@ on
 /;
 
 my @KENLM_PROB_BINS = (0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 0.9, 1);
+my @NADA_PROB_BINS = @KENLM_PROB_BINS;
+
+sub binning {
+    my ($value, @bins) = @_;
+    my $idx = scalar(grep {$_ < $value} @bins);
+    return $bins[$idx];
+}
 
 sub _build_python {
     my $python = Treex::Tool::Python::RunFunc->new();
@@ -48,15 +55,24 @@ CODE
     return $python;
 }
 
+sub get_losses {
+    my ($self, $class) = @_;
+
+    my @losses = map {$_ eq $class ? 0 : 1} @CLASSES;
+    return \@losses;
+}
+
 sub get_shared_feats {
     my ($self, $fr_anode) = @_;
 
-    my ($en_nodes, $ali_types) = Treex::Tool::Align::Utils::get_aligned_nodes_by_filter($fr_anode, {language => 'en', selector => 'src'});
-    my $en_node = shift @$en_nodes;
+    my ($en_anodes, $ali_types) = Treex::Tool::Align::Utils::get_aligned_nodes_by_filter($fr_anode, {language => 'en'});
+    my $en_main_anode = $self->get_main_en_node($fr_anode);
 
     my @feats = ();
-    push @feats, "lemma=".$en_node->lemma;
-    push @feats, $self->kenlm_probs($fr_anode);
+
+    push @feats, $self->get_en_feats($en_main_anode, $en_anodes);
+    push @feats, $self->get_fr_feats($fr_anode);
+    push @feats, $self->get_fr_feats_over_en_antes($en_main_anode);
 
     my $feat_str = join " ", @feats;
    
@@ -71,26 +87,125 @@ sub get_class_feats {
     return \@class_feats;
 }
 
-sub get_losses {
-    my ($self, $class) = @_;
-
-    my @losses = map {$_ eq $class ? 0 : 1} @CLASSES;
-    return \@losses;
+sub get_main_en_node {
+    my ($self, $fr_anode) = @_;
+    my ($replace, $ord) = split /_/, $fr_anode->form;
+    my $bundle = $fr_anode->get_bundle;
+    my $en_atree = $bundle->get_tree('en', 'a', $self->selector);
+    my @en_anodes = $en_atree->get_descendants({ordered => 1});
+    return $en_anodes[$ord];
 }
 
+
+###################### ENGLISH FEATURES ###################################################
+
+sub get_en_feats {
+    my ($self, $en_main_anode, $en_anodes) = @_;
+
+    my @en_feats = ();
+
+    push @en_feats, $self->get_morpho_feats([$en_main_anode], 'en', 1);
+    push @en_feats, $self->get_morpho_feats($en_anodes, 'en');
+    push @en_feats, $self->get_nada_feats($en_main_anode);
+
+    return @en_feats;
+}
+
+# FEAT: [en|fr]_lemma=*
+sub get_morpho_feats {
+    my ($self, $anodes, $lang, $is_main) = @_;
+    return map {$lang.'_'.($is_main ? 'main_' : '').'lemma='.$_->lemma} @$anodes;
+}
+
+# FEAT: en_nada_refer=*
+# FEAT: en_nada_refer_prob=*
+sub get_nada_feats {
+    my ($self, $en_node) = @_;
+
+    my ($en_tnode) = $en_anode->get_referencing_nodes('a/lex.rf');
+    return if (!defined $en_tnode);
+
+    my $is_refer = $en_tnode->wild->{'referential.nada_0.5'};
+    my $is_refer_prob = $en_tnode->wild->{'referential_prob'};
+
+    return if (!defined $is_refer || !defined $is_refer_prob);
+
+    my @feats = ();
+    push @feats, 'en_nada_refer='.$is_refer;
+    push @feats, 'en_nada_refer_prob='.binning($is_refer_prob, @NADA_PROB_BINS);
+
+    return @feats;
+}
+
+#-------------------- COREFERENCE features -----------------------------------------
+
+sub get_en_a_antes {
+    my ($self, $en_anode) = @_;
+
+    my ($en_tnode) = $en_anode->get_referencing_nodes('a/lex.rf');
+    return if (!defined $en_tnode);
+
+    my (@en_t_antes) = $en_tnode->get_coref_chain();
+    my @en_a_antes = grep {defined $_} map {$_->get_lex_anode} @en_t_antes;
+    return @en_a_antes;
+}
+
+
+###################### FRENCH FEATURES ###################################################
+
+sub get_fr_feats {
+    my ($self, $fr_anode) = @_;
+    
+    my @fr_feats = ();
+
+    push @fr_feats, $self->kenlm_probs($fr_anode);
+    return @fr_feats;
+}
+#-------------------- KENLM features ------------------------------------------------
+
+# FEAT: kenlm_w_prob=*
+# FEAT: kenlm_w_rank=*
 sub kenlm_probs {
     my ($self, $fr_anode) = @_;
     my $form = $fr_anode->form;
 
     my $scores = $self->_scores_from_kenlm->{$form};
-    my @feats = map {'kenlm_w_prob='.$_->[0].'_'.kenlm_binning($_->[1])} @$scores;
+    my @feats = map {'kenlm_w_prob='.$_->[0].'_'.binning($_->[1], @KENLM_PROB_BINS)} @$scores;
+    push @feats, map {'kenlm_w_rank='.$scores->[$_][0].'_'.($_+1)} 0 .. $#$scores;
     return @feats;
 }
 
-sub kenlm_binning {
-    my ($value) = @_;
-    my $idx = scalar(grep {$_ < $value} @KENLM_PROB_BINS);
-    return $KENLM_PROB_BINS[$idx];
+##################### FEATURES OVER ENGLISH COREFERENCE ################################
+
+# FEAT: fr_n_antes_over_en_count=*
+# FEAT: fr_closest_ante_over_en_mfeats=*
+# FEAT: fr_closest_ante_over_en_gender=*
+# FEAT: fr_closest_ante_over_en_number=*
+# FEAT: fr_closest_ante_over_en_gender_number=*
+sub get_fr_feats_over_en_antes {
+    my ($self, $en_anode) = @_;
+
+    my @en_a_antes = $self->get_en_a_antes($en_anode);
+    my $fr_antes = Treex::Tool::Align::Utils::aligned_transitively(\@en_a_antes, [{language => 'fr'}]);
+    # select only French nouns
+    my @fr_noun_antes = grep {$_->conll_cpos eq "N"} @$fr_antes;
+
+    return ("fr_n_antes_over_en_count=0") if (!@fr_noun_antes);
+
+    my $fr_closest_ante = $fr_noun_antes[0];
+
+    my @feats = ();
+
+    my $mfeats = $fr_closest_ante->wild->{mfeats};
+    push @feats, "fr_closest_ante_over_en_mfeats=".$mfeats;
+    my @split_mfeats = split /\|/, $mfeats;
+    my ($gender) = map {$_ =~ s/^g=//} grep {$_ =~ /^g=/} @split_mfeats;
+    my ($number) = map {$_ =~ s/^n=//} grep {$_ =~ /^n=/} @split_mfeats;
+    push @feats, "fr_closest_ante_over_en_gender=".$gender;
+    push @feats, "fr_closest_ante_over_en_number=".$number;
+    push @feats, "fr_closest_ante_over_en_gender_number=".$gender.$number;
+
+    return @feats;
 }
 
 before 'process_zone' => sub {
