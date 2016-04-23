568d04226ae7cbbcd12ba8ec4a6390507516f190
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-04-23 12:55:43 +0200

    replace 0/1 losses with weighted losses - this modification replaces both positive and negative classes

---
+++ scripts/add_weighted_losses.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $distr_file = $ARGV[0];
+
+my %losses = ();
+open my $distr_fh, "<", $distr_file;
+while (<$distr_fh>) {
+    chomp $_;
+    next if ($_ =~ /-----/);
+    next if ($_ =~ /TOTAL/);
+    my ($rel, $cum, $cumc, $abs, $label) = split /\s+/, $_;
+    $losses{$label} = $rel;
+}
+close $distr_fh;
+
+while (<STDIN>) {
+    chomp $_;
+    if ($_ =~ /^(\d+):(\d)/) {
+        my $new_loss = $losses{$1};
+        if (!$2) {
+            $new_loss = $new_loss-100;
+        }
+        $_ =~ s/^(\d+):(\d)/$1:$new_loss/;
+    }
+    print $_."\n";
+}
